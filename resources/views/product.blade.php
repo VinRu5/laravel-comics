da880c4aae1f3ed08ae66a95490c17c9b787ee33
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-09-04 16:51:09 +0200

    bozza css page single comic

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -12,82 +12,103 @@ $comicNow = $comics[$comicIndex]
 @endphp
 
 <div class="banner-blue">
-    <div class="img-comics">
-        <img src="{{ $comicNow['thumb'] }}" alt="">
+    <div class="container container-comics">
+        <div class="img-comics">
+            <img src="{{ $comicNow['thumb'] }}" alt="">
+        </div>
     </div>
 </div>
-<div class="container">
-    <div class="row">
-        <div class="col-9">
-            <h2>{{ $comicNow['title'] }}</h2>
-            <div class="green-box">
-                <div>U.S. Price</div>
-                <div>{{ $comicNow['price'] }}</div>
-                <div>AVAILABLE</div>
-                <div>Check Available</div>
-            </div>
-        </div>
-        <div class="col-3">
-            <div>Advertisement</div>
-            <div class="img-adv">
-                <img src="/images/adv.jpg" alt="advertisement">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-6 talent">
-            <h6>Talent</h6>
-            <div>
-                <div>Art by:</div>
-                <div>
-                    @foreach ($comicNow['artists'] as $artist)
-                    @if ($loop->last)
-                    {{ $artist }}
-                    @else
-                    {{ $artist }},
-                    @endif
-                    @endforeach
+<div class="comics-info">
+    <div class="container">
 
-                </div>
-            </div>
-            <div>
-                <div>Written by:</div>
-                <div>
-                    @foreach ($comicNow['writers'] as $writer)
-                    @if ($loop->last)
-                    {{ $writer }}
-                    @else
-                    {{ $writer }},
-                    @endif
-                    @endforeach
+        <div class="row">
+            <div class="col-9">
+                <h2>{{ $comicNow['title'] }}</h2>
+                <div class="green-container">
+                    <div class="green-box green-left">
+                        <div class="green-box-inner">
+                            <div>U.S. Price</div>
+                            <div>{{ $comicNow['price'] }}</div>
 
-                </div>
-            </div>
-        </div>
-        <div class="col-6 specs">
-            <h6>Specs</h6>
-            <div>
-                <div>Series:</div>
-                <div>
-                    {{ $comicNow['series'] }}
-                </div>
-            </div>
-            <div>
-                <div>U.S. Price:</div>
-                <div>
-                    {{ $comicNow['price'] }}
-                </div>
-            </div>
-            <div>
-                <div>On Sale Date:</div>
-                <div>
-                    {{ $comicNow['sale_date'] }}
-                </div>
-            </div>
+                        </div>
+                        <div>AVAILABLE</div>
+                    </div>
+                    <div class="green-box">
+                        <div>Check Available</div>
 
+                    </div>
+                </div>
+                <div class="text-description">
+                    {{ $comicNow['description'] }}
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="text-adv">Advertisement</div>
+                <div class="img-adv">
+                    <img src="/images/adv.jpg" alt="advertisement">
+                </div>
+            </div>
         </div>
     </div>
+</div>
+<div class="comics-details">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 talent">
+                <h6 class="box-info">Talent</h6>
+                <div class="box-info">
+                    <div>Art by:</div>
+                    <div>
+                        @foreach ($comicNow['artists'] as $artist)
+                        @if ($loop->last)
+                        {{ $artist }}
+                        @else
+                        {{ $artist }},
+                        @endif
+                        @endforeach
 
+                    </div>
+                </div>
+                <div class="box-info">
+                    <div>Written by:</div>
+                    <div>
+                        @foreach ($comicNow['writers'] as $writer)
+                        @if ($loop->last)
+                        {{ $writer }}
+                        @else
+                        {{ $writer }},
+                        @endif
+                        @endforeach
+
+                    </div>
+                </div class="box-info">
+            </div>
+            <div class="col-6 specs">
+                <h6 class="box-info">Specs</h6>
+                <div class="box-info">
+                    <div>Series:</div>
+                    <div>
+                        {{ $comicNow['series'] }}
+                    </div>
+                </div>
+                <div class="box-info">
+                    <div>U.S. Price:</div>
+                    <div>
+                        {{ $comicNow['price'] }}
+                    </div>
+                </div>
+                <div class="box-info">
+                    <div>On Sale Date:</div>
+                    <div>
+                        {{ $comicNow['sale_date'] }}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
 
 </div>
 
